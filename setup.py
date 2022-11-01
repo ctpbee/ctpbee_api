8a757a5cd351dab93ead554a413130fb4dcd7233
somewheve <somewheve@gmail.com>
2022-11-01 16:49:54 +0800

    add support for 3.11

--- setup.py
+++ setup.py
@@ -127,7 +127,7 @@ pkgs = ["ctpbee_api", 'ctpbee_api.ctp', "ctpbee_api.ctp_mini", ]
 
 setup(
     name='ctpbee_api',
-    version=0.32,
+    version=0.33,
     description="single CTP API support, From VNPY",
     author='somewheve',
     long_description=long_description,
